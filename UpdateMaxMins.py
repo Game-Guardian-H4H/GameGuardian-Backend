9cd168077fa7bd86a53a26bddf40d634a1058983
TruptiJoshi <trupity@gmail.com>
2023-02-18 14:30:08 -0800

    Add files via upload

---
+++ UpdateMaxMins.py
@@ -0,0 +1,25 @@
+in_memory_datastore = {
+    "Max_mins": {"max": "60"}
+}
+
+
+@app.route('/gameSafe/maxMins', methods=['GET'])
+def get_time():
+    return {"MaxMins": list(in_memory_datastore.values())}
+
+
+@app.route('/update', methods=['POST'])
+def update_time():
+    # Get the updated data from the request body
+    updated_data = request.get_json()
+
+    # Update the original data with the new data
+    for key, value in updated_data.items():
+        my_data[key] = value
+
+    # Return a success message
+    return 'Time updated successfully!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
