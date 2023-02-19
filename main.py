815e68214a76717fc23079f7ce91d61350ab9722
Mohan Raj <mohanraj746007@gmail.com>
2023-02-18 21:14:35 -0800

    api end point connection fix

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from flask import Flask, request, jsonify #added to top of file
-#from flask_cors import CORS #added to top of file
+from flask_cors import CORS, cross_origin
 
 from Create_user import insert_user
 from GetAllUsers import get_All_Users
 from GetUserByID import get_user_by_id
 from UpdateUser import update_user, delete_user
 
-
 app = Flask(__name__)
+# enable CORS
+cors = CORS(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/pausegame')
+@app.route('/hello')
+def helloworld():
+    return 'Hello, World!'
+
+@app.route('/api/pausegame', methods=['GET'])
 def pausegame():
     print("pausegame")
     return 'noooo!'
-
-app = Flask(__name__)
-#CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/api/users', methods=['GET'])
 def api_get_users():
@@ -43,7 +46,6 @@ def api_update_user():
 def api_delete_user(user_id):
     return jsonify(delete_user(user_id))
 
-if __name__ == "__main__":
-    #app.debug = True
-    #app.run(debug=True)
-    app.run(debug=True, ssl_context='adhoc') #run app
+# if __name__ == "__main__":
+    # app.run(debug=True, ssl_context='adhoc') #run app
+    app.run(debug=True)  # run app
