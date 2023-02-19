e1a046ba2c6f870c55ae12b86f4e3794b86638a1
TruptiJoshi <trupity@gmail.com>
2023-02-18 21:16:16 -0800

    Adding main.py

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from flask_cors import CORS, cross_origin
 
 from Create_user import insert_user
 from GetAllUsers import get_All_Users
+from GetAllowedTime import get_Allowed_Time
+from GetPauseState import get_Pause_State
 from GetUserByID import get_user_by_id
 from UpdateUser import update_user, delete_user
+from PutPlayedTime import put_Played_Time
+
 
 app = Flask(__name__)
-# enable CORS
-cors = CORS(app)
-
 
 @app.route('/')
+@cross_origin
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/hello')
-def helloworld():
-    return 'Hello, World!'
+@app.route('/api/pausegame/<user_id>', methods=['GET'])
+def pausegame(user_id):
+    return jsonify(get_Pause_State())
 
-@app.route('/api/pausegame', methods=['GET'])
-def pausegame():
-    print("pausegame")
-    return 'noooo!'
+app = Flask(__name__)
+#CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/api/users', methods=['GET'])
 def api_get_users():
@@ -46,6 +46,15 @@ def api_update_user():
 def api_delete_user(user_id):
     return jsonify(delete_user(user_id))
 
-# if __name__ == "__main__":
-    # app.run(debug=True, ssl_context='adhoc') #run app
-    app.run(debug=True)  # run app
+@app.route('/api/users/getAllowedTime/<user_id>',  methods = ['GET'])
+def api_user_allowed_time(user_id):
+    return jsonify(get_Allowed_Time(user_id))
+
+@app.route('/api/users/putPlayedTime/<user_id, played_time>',  methods = ['PUT'])
+def api_user_played_time(user_id, played_time):
+    return jsonify(put_Played_Time(user_id, played_time))
+
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(debug=True)
+    app.run(ssl_context=('/Users/abhishekpatil/certificates/server.crt', '/Users/abhishekpatil/certificates/server.key')) #run app
