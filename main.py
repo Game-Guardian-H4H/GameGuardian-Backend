3c432adf36b31d72780e3a9ae683aa9576a50857
Mohan Raj <mohanraj746007@gmail.com>
2023-02-18 23:10:32 -0800

    health status check endpoint added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify #added to top of file
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 from Create_user import insert_user
 from GetAllUsers import get_All_Users
@@ -8,6 +9,9 @@ from GetUserByID import get_user_by_id
 from UpdateUser import update_user, delete_user
 from PutPlayedTime import put_Played_Time
 from setPauseState import set_Pause_State
+import sqlite3
+
+
 
 app = Flask(__name__)
 
@@ -25,7 +29,7 @@ def setPauseState(user_id):
 
 app = Flask(__name__)
 
-@app.route('/api/users', methods=['GET'])
+@app.route('/api/users/all', methods=['GET'])
 def api_get_users():
     return jsonify(get_All_Users())
 
@@ -51,11 +55,22 @@ def api_delete_user(user_id):
 def api_user_allowed_time(user_id):
     return jsonify(get_Allowed_Time(user_id))
 
-@app.route('/api/users/putPlayedTime/<user_id, played_time>',  methods = ['PUT'])
+@app.route('/api/users/putPlayedTime/<int:user_id>/<int:played_time>',  methods=['PUT'])
 def api_user_played_time(user_id, played_time):
     return jsonify(put_Played_Time(user_id, played_time))
 
+@app.route('/api/server/healthStatus',  methods = ['GET'])
+def checkHealthStatus():
+    conn = sqlite3.connect('database.db')
+    # ping the database to check if it's alive
+    cursor = conn.cursor()
+    cursor.execute('SELECT SQLITE_VERSION()')
+    data = cursor.fetchone()
+    res = {"health_status": "orange"}
+    if data:
+        res["health_status"] = "green"
+    return jsonify(res)
+
 if __name__ == "__main__":
     #app.debug = True
-    #app.run(debug=True)
-    app.run(ssl_context=('/Users/abhishekpatil/certificates/server.crt', '/Users/abhishekpatil/certificates/server.key')) #run app
+    app.run(debug=True)
